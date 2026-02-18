25d859c5e3c346f9191a2767a94dd4c7bc9c19a0
Max Zhang <142354778+maxh119Z@users.noreply.github.com>
2026-02-18 09:57:23 -0800

    Update reproduce.sh

--- custom_automation/reproduce.sh
+++ custom_automation/reproduce.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
-# Usage: ./reproduce.sh
+# Usage: HF_TOKEN="your_key_here" ./reproduce.sh
 # Reproduces the add descriptions section of our paper.
 
 set -e
 cd "$(dirname "$0")"
 
+# 1. Setup virtual environment and install dependencies
 python3 -m venv venv_auto-circuits
 source venv_auto-circuits/bin/activate
 pip install .
+#pip install --force-reinstall torch torchvision torchaudio   (Mainly a Runpod Issue).
 
+# 1_A. Login to Huggingface for gated models.
+echo "Logging in to Huggingface"
+python huggingface_login.py
 cd custom_automation
+
+
+# 2. Descriptions for each feature.
+echo "------------------------------------------------"
+
+echo "2_A. Fetching Features with influence <= PRUNING_THRESHOLD"
+echo "------------------------------------------------"
+# This script downloads all feature information from HF based on your graph at specific pruning level.
+python fetch_all_activating_text.py
+
+echo "------------------------------------------------"
+echo "2_B. Generating Descriptions - Transluce Llama"
+echo "------------------------------------------------"
+# This script runs the local LLM to describe features. A decent GPU is required to run the 8B model.
+python add_description.py
+
+echo "------------------------------------------------"
+echo "2_C. Merging to Graph (Update Website)"
+echo "------------------------------------------------"
+# This script injects the descriptions into test-run.json where website features are stored. 
+python push_to_website.py
+
+echo "------------------------------------------------"
+echo "SUCCESS! Step 2 finished."
+echo "Refresh your localhost and clear browser cache to see updates."
